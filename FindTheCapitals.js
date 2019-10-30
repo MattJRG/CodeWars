99a959cfd0a8757fc279cd5112e3a3a46fb91e02
Matthew Gathercole <30979900+MattJRG@users.noreply.github.com>
2019-10-30 13:47:35 +0000

    Create FindTheCapitals.js

---
+++ FindTheCapitals.js
@@ -0,0 +1,5 @@
+// Description
+/*
+Write a function that takes a single string (word) as argument. The function must return an ordered list containing the indexes of all capital letters in the string.
+*/
+
