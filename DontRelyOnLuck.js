168c2a6c6db19d4d221e7f50a07d3d64aa1dea19
Matthew Gathercole <30979900+MattJRG@users.noreply.github.com>
2019-10-30 14:01:05 +0000

    Create DontRelyOnLuck.js

---
+++ DontRelyOnLuck.js
@@ -0,0 +1,32 @@
+// URL: https://www.codewars.com/kata/dont-rely-on-luck/train/javascript
+
+// Description
+
+/*
+The test fixture I use for this kata is pre-populated.
+
+It will compare your guess to a random number generated using:
+
+Math.floor(Math.random() * 100 + 1)
+You can pass by relying on luck or skill but try not to rely on luck.
+
+"The power to define the situation is the ultimate power." - Jerry Rubin
+
+Good luck! 
+*/
+
+// Code
+
+/*
+
+Math.floor = function () {
+  return 10;
+}
+
+const luckyNumber = Math.floor(Math.random() * 100 + 1)
+
+var guess = 10
+
+console.log(luckyNumber == guess)
+
+*/
