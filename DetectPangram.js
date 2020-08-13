4c15377ee6b58361a40744d6878fcc877bb6f11d
Matthew Gathercole <30979900+MattJRG@users.noreply.github.com>
2020-08-13 08:52:53 +0100

    Create DetectPangram.js

---
+++ DetectPangram.js
@@ -0,0 +1,37 @@
+// URL: https://www.codewars.com/kata/545cedaa9943f7fe7b000048
+
+// Description
+/*
+A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+*/
+
+// Code
+
+// Solution 1 - using object
+
+function isPangram(string){
+  let lookup = {
+    a: false, b: false, c: false, d: false, e: false, f: false, g: false, h: false, i: false, j: false, k: false, l: false, m: false,
+    n: false, o: false, p: false, q: false, r: false, s: false, t: false, u: false, v: false, w: false, x: false, y: false, z: false,
+  }
+  str = str.toLowerCase();
+  for(let i = 0; i < string.length; i++) {
+    let char = string[i];
+    if (lookup[char] === false) {
+      lookup[char] = true;
+    }
+  }
+  return !Object.values(lookup).includes(false);
+}
+
+// Solution 2 - using string
+/*
+function isPangram(string){
+  string = string.toLowerCase();
+  return "abcdefghijklmnopqrstuvwxyz".split("").every(function(x){
+    return string.indexOf(x) !== -1;
+  });
+}
+*/
